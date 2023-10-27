a8cc99736e29a8b8526fc165d4258d5dff6dd7ed
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2023-10-27 16:12:51 -0500

    Fixes and some improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,11 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav__toggle','nav-menu')
+
+/*GSAP ANIMATION*/
+gsap.from('.home__title', {opacity: -1, duration: 1.5, delay:1})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1, x: 100})
+gsap.from('.home__information', {opacity: 0, duration: 1, delay:1.2, y: -30})
+
+gsap.from('.box__medium', {opacity: 0, duration: 1.5, delay:1.3, y: -50})
+gsap.from('.box__small', {opacity: -1, duration: 1.5, delay:1.4, y: -50})
